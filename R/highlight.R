ac4ced5a8c2cc642f4fdee323fe8cb5d073f1c5c
Yihui Xie <xie@yihui.name>
2013-08-31 01:51:36 -0500

    a better approach to preserving blank lines; this will make sure the last token on a line can be merged to previous tokens if it is the same class as them

--- R/highlight.R
+++ R/highlight.R
@@ -147,6 +147,8 @@ hilight_one = function(code, format, markup, escape_fun) {
 
   # record how many blank lines after each token
   blanks = c(pmax(res$line1[-1] - res$line2[-nrow(res)] - 1, 0), 0)
+  # add line breaks to the 8th column
+  res = cbind(res, mapply(spaces, blanks, '\n'))
 
   # e.g. a string spans across multiple lines; now need to replace line1 with
   # line2 so that we know the starting and ending positions of spaces; e.g. turn
@@ -154,10 +156,6 @@ hilight_one = function(code, format, markup, escape_fun) {
   for (i in which(res$line1 != res$line2)) {
     res$line1[res$line1 == res$line1[i]] = res$line2[i]
   }
-
-  # add line breaks back
-  if (any(blanks > 0))
-    res[, 7] = paste(res[, 7], mapply(spaces, blanks, '\n'), sep = '')
 
   unlist(lapply(split(res, res$line1), function(d) {
     # merge adjacent tokens of the same type so that the output is cleaner
@@ -170,7 +168,7 @@ hilight_one = function(code, format, markup, escape_fun) {
     # add 0 and remove col[n, 2] to get start/end positions of spaces
     col = matrix(head(c(0, t(col)), -1), ncol = 2, byrow = TRUE)
     paste(mapply(spaces, col[, 2] - col[, 1] - 1), d[, 6], d[, 'text'], d[, 7],
-          sep = '', collapse = '')
+          d[, 8], sep = '', collapse = '')
   }), use.names = FALSE)
 }
 #' @export
