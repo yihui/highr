4b81502b5d161b500f31b38fff6765596362f625
Yu <fengyu098@gmail.com>
2024-05-25 11:51:27 +0800

    Rewrite the form

--- tests/testit/test-utils.R
+++ tests/testit/test-utils.R
@@ -1,15 +1,13 @@
 library(testit)
 
-assert(
-  'escape_latex() sanitizes backslashes and {}',
-  escape_latex('\\') == '\\textbackslash{}',
-  escape_latex('\\{}') == '\\textbackslash{}\\{\\}',
-  escape_latex('{\\}') == '\\{\\textbackslash{}\\}',
-  escape_latex('~!@#$%^&*()') == '~!@#$%^&*()'
-)
+assert('escape_latex() sanitizes backslashes and {}', {
+  (escape_latex('\\') %==% '\\textbackslash{}')
+  (escape_latex('\\{}') %==% '\\textbackslash{}\\{\\}')
+  (escape_latex('{\\}') %==% '\\{\\textbackslash{}\\}')
+  (escape_latex('~!@#$%^&*()') %==% '~!@#$%^&*()')
+})
 
-assert(
-  'escape_html() escapes HTML chars',
-  escape_html('&"<>') == '&amp;&quot;&lt;&gt;',
-  escape_html('~!@#$%^&*()') == '~!@#$%^&amp;*()'
-)
+assert('escape_html() escapes HTML chars', {
+  (escape_html('&"<>') %==% '&amp;&quot;&lt;&gt;')
+  (escape_html('~!@#$%^&*()') %==% '~!@#$%^&amp;*()')
+})
